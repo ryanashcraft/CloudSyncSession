c20b9c7b62810277c62b54c5089caa9314d80c59
Ryan Ashcraft <ryan@ryanashcraft.me>
2020-07-28 15:41:27 -0700

    Remove Cirrus dependency

--- Package.swift
+++ Package.swift
@@ -19,16 +19,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/jayhickey/Cirrus.git", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CloudSyncSession",
-            dependencies: [
-                .product(name: "CloudKitCodable", package: "Cirrus")
-            ]
+            dependencies: []
         ),
         .testTarget(
             name: "CloudSyncSessionTests",
