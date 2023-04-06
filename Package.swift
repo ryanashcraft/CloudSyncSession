2c4e9fa3436179cf50695b4485b2eb4c1b284902
Ryan Ashcraft <ryan@ryanashcraft.me>
2023-04-06 07:45:06 -0700

    Declare support for macOS 12

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,9 @@ import PackageDescription
 let package = Package(
     name: "CloudSyncSession",
     platforms: [
-        .iOS("15.0"),
-        .watchOS("8.0"),
+        .iOS(.v15),
+        .watchOS(.v8),
+        .macOS(.v12),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
