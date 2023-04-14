883af7c4d96d9cf03e2d68ac064438422c63e4a4
Ryan Ashcraft <ryan@ryanashcraft.me>
2023-04-14 13:20:02 -0700

    Remove comments in Package.swift

--- Package.swift
+++ Package.swift
@@ -11,18 +11,13 @@ let package = Package(
         .macOS(.v12),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CloudSyncSession",
             targets: ["CloudSyncSession"]
         ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CloudSyncSession",
             dependencies: []
