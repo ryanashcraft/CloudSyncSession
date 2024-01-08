4de1efc17f50233e07bb88911ac7708705ee00c1
Ryan Ashcraft <ryan@ryanashcraft.me>
2024-01-08 15:30:26 -0800

    Retry failed requests where every partial error is serviceUnavailable

--- Sources/CloudSyncSession/Middleware/ErrorMiddleware.swift
+++ Sources/CloudSyncSession/Middleware/ErrorMiddleware.swift
@@ -118,17 +118,31 @@ struct ErrorMiddleware: Middleware {
                 case let .modify(operation):
                     // Supported modify partial failures: batchRequestFailed and serverRecordChanged
 
-                    guard let partialErrors = ckError.partialErrorsByItemID as? [CKRecord.ID: Error] else {
+                    guard let partialErrorsByRecordID = ckError.partialErrorsByItemID as? [CKRecord.ID: Error] else {
                         return .halt(error)
                     }
 
-                    let recordIDsNotSavedOrDeleted = Set(partialErrors.keys)
+                    let recordIDsNotSavedOrDeleted = Set(partialErrorsByRecordID.keys)
+
+                    let partialErrors = partialErrorsByRecordID.compactMap { $0.value as? CKError }
+
+                    let allErrorsAreRetryable = partialErrors
+                        .allSatisfy { error in
+                            if case .serviceUnavailable = error.code {
+                                return true
+                            } else {
+                                return false
+                            }
+                        }
+
+                    if allErrorsAreRetryable {
+                        let retryAfter = partialErrors.compactMap { $0.retryAfterSeconds }.max()
+
+                        return .retry(work, error, retryAfter)
+                    }
 
                     let unhandleableErrorsByItemID = partialErrors
-                        .filter { _, error in
-                            guard let error = error as? CKError else {
-                                return true
-                            }
+                        .filter { error in
 
                             switch error.code {
                             case .batchRequestFailed, .serverRecordChanged, .unknownItem:
@@ -145,7 +159,7 @@ struct ErrorMiddleware: Middleware {
 
                     // All IDs for records that are unknown by the container (probably deleted by another client)
                     let unknownItemRecordIDs = Set(
-                        partialErrors
+                        partialErrorsByRecordID
                             .filter { _, error in
                                 if let error = error as? CKError, error.code == .unknownItem {
                                     return true
@@ -158,7 +172,7 @@ struct ErrorMiddleware: Middleware {
 
                     // All IDs for records that failed to be modified due to some other error in the batch modify operation
                     let batchRequestFailedRecordIDs = Set(
-                        partialErrors
+                        partialErrorsByRecordID
                             .filter { _, error in
                                 if let error = error as? CKError, error.code == .batchRequestFailed {
                                     return true
@@ -171,14 +185,13 @@ struct ErrorMiddleware: Middleware {
 
                     // All errors for records that failed because there was a conflict
                     let serverRecordChangedErrors = partialErrors
-                        .filter { _, error in
-                            if let error = error as? CKError, error.code == .serverRecordChanged {
+                        .filter { error in
+                            if error.code == .serverRecordChanged {
                                 return true
                             }
 
                             return false
                         }
-                        .values
 
                     // Resolved records
                     let resolvedConflictsToSave = serverRecordChangedErrors
